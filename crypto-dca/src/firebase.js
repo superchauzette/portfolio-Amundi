d215cc4fbbc7c2aebb90cea7084a21c7d09a0566
RomainD2 <demaison.romain.forum@gmail.com>
2022-11-03 20:40:27 +0100

    Update firebase.js
    Modifications jakwarrior du forum h-fr

--- crypto-dca/src/firebase.js
+++ crypto-dca/src/firebase.js
@@ -1,7 +1,7 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/functions";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/functions";
 
 let firebaseConfig = {
   apiKey: "AIzaSyDWuYyO2SKjbdrXCUhkWrAgAy8xPCEMEFc",
